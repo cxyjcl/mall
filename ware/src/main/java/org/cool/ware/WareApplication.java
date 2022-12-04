b18ca83e0264cbc033222256e0c630f3a96d0d78
jeannie <jeannie.jiang@jeanniedeMacBook-Pro.local>
2022-12-04 20:31:45 +0800

    create a scaffold

---
+++ ware/src/main/java/org/cool/ware/WareApplication.java
@@ -0,0 +1,13 @@
+package org.cool.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareApplication.class, args);
+	}
+
+}
